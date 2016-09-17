b94d52361335a31f04fb16db38386847cb48c149
Long Sun <spritesun@gmail.com>
2016-09-17 12:19:22 +1000

    Initial Commit

---
+++ Playground/Playground.h
@@ -0,0 +1,19 @@
+//
+//  Playground.h
+//  Playground
+//
+//  Created by Long Sun on 17/09/2016.
+//  Copyright © 2016 Long Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Playground.
+FOUNDATION_EXPORT double PlaygroundVersionNumber;
+
+//! Project version string for Playground.
+FOUNDATION_EXPORT const unsigned char PlaygroundVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Playground/PublicHeader.h>
+
+
